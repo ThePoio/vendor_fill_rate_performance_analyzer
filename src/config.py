765e52e8b72f6a5b8dcf9f0647b436655c7225b5
ThePoio <jose.torres5511@alumnos.udg.mx>
2026-06-24 17:52:36 -0600

    Cambio de API de cloude a OpenAI

--- src/config.py
+++ src/config.py
@@ -1,17 +1,17 @@
 import os 
 from dotenv import load_dotenv
-from langchain_anthropic import ChatAnthropic  
+from langchain_openai import ChatOpenAI  
 
 #Carga de la credencial del arciho .env
 load_dotenv()
 
 def inicializar_llm():
-    """Inicializa el modelo de lenguaje de Anthropic utilizando la clave API almacenada en las variables de entorno."""
+    """Inicializa el modelo de lenguaje de OpenAI utilizando la clave API almacenada en las variables de entorno."""
 
-    #Se usara claude-3-5-sonnet
+    #Se usara gpt-4o-mini
 
-    return ChatAnthropic(
-        model="claude-3-5-sonnet",
+    return ChatOpenAI(
+        model="gpt-4o-mini",
         temperature=0.3,
         timeout=None,
         max_retries=2,
